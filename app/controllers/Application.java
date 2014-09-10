0fd13fe79e64b39c686997ecbb0b6d6dc95c328e
Harald Eilert <harald.eilert@gmail.com>
2014-09-10 16:52:29 +0200

    refactor

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,29 +11,32 @@ import java.util.List;
 
 public class Application extends Controller {
 
+    public static final String DATA = "data";
+
     public static Result index() {
-        String url = CurrentUser.getAuthorizeURL();
+        String url = SpotifyWebApi.getAuthorizeURL();
         return redirect(url);
     }
 
     public static Result callback(String code) {
-        String accessToken = CurrentUser.getAccessToken(code);
+        String accessToken = SpotifyWebApi.getAccessToken(code);
         return ok(views.html.playlists.render(accessToken));
     }
 
     public static Result getPlayList(String accessToken) {
-        User user = CurrentUser.getCurrentUser(accessToken);
-        Page<SimplePlaylist> playlistPage = CurrentUser.getPlaylistsForUser(user.getId(), accessToken);
+        User user = SpotifyWebApi.getCurrentUser(accessToken);
+        Page<SimplePlaylist> playlistPage = SpotifyWebApi.getPlaylistsForUser(user.getId(), accessToken);
         JSONArray mainArray = new JSONArray();
 
         for (SimplePlaylist playlist : playlistPage.getItems()) {
-            Page<PlaylistTrack> page = CurrentUser.getTracksFromPlayList(user.getId(), playlist.getId(), accessToken);
+            Page<PlaylistTrack> page = SpotifyWebApi.getTracksFromPlayList(user.getId(), playlist.getId(), accessToken);
             if (page != null) {
                 try {
                     List<PlaylistTrack> playlistTracks = page.getItems();
                     for (PlaylistTrack playlistTrack : playlistTracks) {
                         List<SimpleArtist> simpleArtistList = playlistTrack.getTrack().getArtists();
                         String artists = "";
+                        //Loop through the artist list and make a string
                         for (SimpleArtist simpleArtist : simpleArtistList) {
                             if (!"".equals(artists))
                                 artists += ", ";
@@ -49,14 +52,14 @@ public class Application extends Controller {
                     }
 
                 } catch (Exception e) {
-                   Logger.error("Something went wrong!" + e.getMessage());
+                   Logger.error(e.getMessage());
                 }
-            } else {
-                Logger.error("*****page is null: " + playlist.getName());
+            }else{
+                Logger.info("Track for playlist " + playlist.getName() +" cannot be fetched since it's collaborative.");
             }
         }
         JSONObject data = new JSONObject();
-        data.put("data", mainArray);
+        data.put(DATA, mainArray);
         return ok(data.toJSONString());
     }
 }
