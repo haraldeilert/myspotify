b7d83e469d5043e8ebfcb3efb04f823d239f6a40
Harald Eilert <harald.eilert@gmail.com>
2014-09-24 11:22:58 +0200

    working

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,10 +4,8 @@ import com.wrapper.spotify.models.*;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import play.Logger;
-import play.libs.F;
 import play.mvc.Controller;
 import play.mvc.Result;
-import play.mvc.WebSocket;
 
 import java.util.List;
 
@@ -140,21 +138,5 @@ public class Application extends Controller {
         JSONObject data = new JSONObject();
         data.put(DATA, mainArray);
         return ok(data.toJSONString());
-    }
-
-    public static WebSocket<String> sockHandler() {
-        return new WebSocket<String>() {
-            // called when the websocket is established
-            public void onReady(WebSocket.In<String> in, WebSocket.Out<String> out) {
-                // register a callback for processing instream events
-                in.onMessage(new F.Callback<String>() {
-                    public void invoke(String event) {
-                       Logger.info("teset");
-                       out.write("asdf");
-                    }
-                });
-
-            }
-        };
     }
 }
