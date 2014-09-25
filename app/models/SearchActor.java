84aa6eff09d10c265a6fd8757657070f8767cbc5
Harald Eilert <harald.eilert@gmail.com>
2014-09-25 13:47:27 +0200

    Lambda, yeeahhgit status

--- app/models/SearchActor.java
+++ app/models/SearchActor.java
@@ -9,32 +9,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Diversify on 2014-09-23.
+ * Created by Harald on 2014-09-23.
  */
 public class SearchActor extends UntypedActor {
     // collect all websockets here
     private static List<WebSocket.Out<String>> connections = new ArrayList<WebSocket.Out<String>>();
 
     public static void start(WebSocket.In<String> in, WebSocket.Out<String> out) {
-
         Logger.debug("Add connection");
         connections.add(out);
 
-        in.onMessage(new F.Callback<String>() {
-            public void invoke(String event) {
-                SearchActor.notifyAll(event);
-            }
-        });
-
-        in.onClose(new F.Callback0() {
-            public void invoke() {
-                Logger.debug("Connection closed");
-            }
-        });
+        in.onMessage(event -> notifyAll(event));
+        in.onClose(() -> Logger.debug("Connection closed"));
     }
 
     // Iterate connection list and write incoming message
-    public static void notifyAll(String message) {
+    private static void notifyAll(String message) {
         for (WebSocket.Out<String> out : connections) {
             out.write(message);
         }
