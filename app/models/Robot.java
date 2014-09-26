87648be6436fb852141da3181f31df22554c66ff
Harald Eilert <harald.eilert@gmail.com>
2014-09-26 14:18:29 +0200

    Robot at work

--- app/models/Robot.java
+++ app/models/Robot.java
@@ -7,6 +7,7 @@ import scala.concurrent.duration.Duration;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class Robot {
+
     public Robot(ActorRef searchActor) {
         // Make the robot talk every 30 seconds
         Akka.system().scheduler().schedule(
